e6ebede82d1c9274bfd80c14421925f0af035921
aubustou <survivalfr@yahoo.fr>
2022-06-04 15:53:21 +0000

    Bump to 0.6

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='botosc',
-    version='0.5',
-    packages=['botosc', 'botosc_gen'],
-    url='',
-    license='MIT',
-    author='Aubustou',
-    author_email='survivalfr@yahoo.fr',
-    description='Get dataclass objects out of Outscale API',
+    name="botosc",
+    version="0.6",
+    packages=["botosc", "botosc_gen"],
+    url="",
+    license="MIT",
+    author="Aubustou",
+    author_email="survivalfr@yahoo.fr",
+    description="Get dataclass objects out of Outscale API",
     install_requires=[
         "requests",
         "apischema",
